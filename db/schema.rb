dcf66a8b5c800529531c2b615097043fa485502f
yu wong <sakuragi.yu@gmail.com>
2025-05-21 10:14:25 +0100

    seeds added

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.2].define(version: 2025_05_20_164742) do
+ActiveRecord::Schema[7.2].define(version: 2025_05_20_151559) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,10 +40,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_20_164742) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-=======
-ActiveRecord::Schema[7.2].define(version: 2025_05_20_145309) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  end
 
   create_table "bookings", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -55,7 +51,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_20_145309) do
     t.datetime "updated_at", null: false
     t.index ["car_id"], name: "index_bookings_on_car_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
->>>>>>> master
   end
 
   create_table "cars", force: :cascade do |t|
@@ -81,12 +76,9 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_20_145309) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-<<<<<<< HEAD
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-=======
   add_foreign_key "bookings", "cars"
   add_foreign_key "bookings", "users"
->>>>>>> master
   add_foreign_key "cars", "users"
 end
