ffd7b1e85c39e6cfb7a12e4f3bc540a3c9a5c528
Paul Foma Ndieye <paulfoma6@gmail.com>
2025-05-23 16:06:59 +0100

    car-show

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,5 +1,4 @@
 import { Controller } from "@hotwired/stimulus"
-import mapboxgl from 'mapbox-gl'
 
 export default class extends Controller {
   static values = {
@@ -8,45 +7,62 @@ export default class extends Controller {
   }
 
   connect() {
-    mapboxgl.accessToken = this.apiKeyValue
+    if (typeof mapboxgl === 'undefined') {
+      console.error("Mapbox GL JS is not defined. Ensure it's loaded correctly.");
+      return;
+    }
+
+    mapboxgl.accessToken = this.apiKeyValue;
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: 'mapbox://styles/mapbox/streets-v11'
-    })
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-51.5072, 0.1276], // Default center
+      zoom: 12
+    });
 
-    this.#addMarkersToMap()
-    this.#fitMapToMarkers()
+    this.map.addControl(new mapboxgl.NavigationControl());
+    this.map.addControl(
+      new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl,
+        marker: false
+      })
+    );
+
+    this.map.on('load', () => {
+      this.#addMarkersToMap();
+      this.#fitMapToMarkers();
+    });
   }
 
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.info_window)
+      const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(marker.info_window_html);
 
-      new mapboxgl.Marker()
-        .setLngLat([ marker.lng, marker.lat ])
+      const customMarker = document.createElement("div");
+      customMarker.className = "map-marker";
+      customMarker.innerHTML = marker.marker_html;
+
+      new mapboxgl.Marker(customMarker)
+        .setLngLat([marker.lng, marker.lat])
         .setPopup(popup)
-        .addTo(this.map)
-    })
+        .addTo(this.map);
+    });
   }
 
   #fitMapToMarkers() {
-    const bounds = new mapboxgl.LngLatBounds()
-    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
-  }
-  #addMarkersToMap() {
-    this.markersValue.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.info_window)
+    if (this.markersValue.length === 0) return;
 
-      // Center map on marker
-      this.map.setCenter([marker.lng, marker.lat])
-      this.map.setZoom(14)
+    const bounds = new mapboxgl.LngLatBounds();
 
-      new mapboxgl.Marker()
-        .setLngLat([ marker.lng, marker.lat ])
-        .setPopup(popup)
-        .addTo(this.map)
-    })
+    this.markersValue.forEach(marker => {
+      bounds.extend([marker.lng, marker.lat]);
+    });
+
+    this.map.fitBounds(bounds, {
+      padding: 50,
+      maxZoom: 15
+    });
   }
 }
